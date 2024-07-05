9b69098fed4bd9e2dfec1823259252e5f0068574
panakir <panakir.webdev@gmail.com>
2024-07-05 23:46:00 +0300

    feat: add reload button into Fallback component (Fri, Jul 05, 2024 23:45)

--- src/assets/components/errorBoundary/Fallback.tsx
+++ src/assets/components/errorBoundary/Fallback.tsx
@@ -9,6 +9,7 @@ export class Fallback extends Component {
           Oops! Something went wrong! Please, reload website!
         </p>
         <h2 className={styles.title}>May the Force be with you</h2>
+        <button onClick={() => window.location.reload()}>reload</button>
       </div>
     );
   }
