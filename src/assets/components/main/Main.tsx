4b9228caad0f203316e5da8c769ca59a520cf6bc
panakir <panakir.webdev@gmail.com>
2024-07-12 13:11:44 +0300

    feat: implement Pagination component (Fri, Jul 12, 2024 13:12)

--- src/assets/components/main/Main.tsx
+++ src/assets/components/main/Main.tsx
@@ -3,31 +3,58 @@ import { Search } from "../search/Search";
 import { Results } from "../results/Results";
 import { Character } from "../../shared/types";
 import { getAllCharacters, getFilteredCharacters } from "../../shared/api";
+import { Pagination } from "../pagination/Pagination";
 
 export const Main = (): ReactNode => {
   const [searchResult, setSearchResult] = useState<Character[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [qtyCharacters, setQtyCharacters] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const handleSearchRequest = useCallback(async (): Promise<void> => {
     setIsLoading(true);
     const query = localStorage.getItem("searchTerm");
+
     if (query) {
-      const filteredCharacters = await getFilteredCharacters(query);
-      setSearchResult(filteredCharacters);
+      const filteredCharacters = await getFilteredCharacters(
+        query,
+        currentPage
+      );
+      setSearchResult(filteredCharacters.results);
+      setQtyCharacters(filteredCharacters.count);
       setIsLoading(false);
     } else {
-      const characters = await getAllCharacters();
-      setSearchResult(characters);
+      const characters = await getAllCharacters(currentPage);
+      setQtyCharacters(characters.count);
+      setSearchResult(characters.results);
       setIsLoading(false);
     }
-  }, []);
+  }, [currentPage]);
 
   useEffect(() => {
     handleSearchRequest();
   }, [handleSearchRequest]);
 
+  const handleSelectedPage = (page: number): void => {
+    setCurrentPage(page);
+  };
+
+  const handlePreviousPage = (page: number): void => {
+    setCurrentPage(page - 1);
+  };
+  const handleNextPage = (page: number): void => {
+    setCurrentPage(page + 1);
+  };
+
   return (
     <main>
+      <Pagination
+        qtyCharacters={qtyCharacters}
+        currentPage={currentPage}
+        handlePreviousPage={handlePreviousPage}
+        handleSelectedPage={handleSelectedPage}
+        handleNextPage={handleNextPage}
+      />
       <Search handleSearch={handleSearchRequest} />
       {isLoading ? (
         <div className="loader">
