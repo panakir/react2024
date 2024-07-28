6ac0b881be71a3532729c8c3c18fb404328b3182
panakir <panakir.webdev@gmail.com>
2024-07-28 13:23:58 +0300

    refactor: refactor router file (delete loader for details component), delete api file (Sun, Jul 28, 2024 13:23)

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,20 +2,7 @@ import { App } from "@/components/app/App";
 import { Details } from "@/components/details/Details";
 import { Fallback } from "@/components/layouts/errorBoundary/Fallback";
 import { NotFound } from "@/components/layouts/not-found/NotFound";
-import { getCharacter } from "@/shared/api";
-import {
-  LoaderFunction,
-  LoaderFunctionArgs,
-  createBrowserRouter,
-} from "react-router-dom";
-
-const loaderDetails: LoaderFunction = async ({
-  params,
-}: LoaderFunctionArgs) => {
-  const { id } = params;
-  const response = id && (await getCharacter(id));
-  return response;
-};
+import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
   {
@@ -30,7 +17,6 @@ export const router = createBrowserRouter([
       {
         path: "details/:id",
         element: <Details />,
-        loader: loaderDetails,
       },
     ],
   },
