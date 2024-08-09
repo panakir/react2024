4bb155412d8c6f84eb5653296c269b8f17ac8142
panakir <panakir.webdev@gmail.com>
2024-08-09 21:37:46 +0300

    feat: add not found route and error page route (Fri, Aug 09,  2024 21:37)

---
+++ src/app/error.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { Fallback } from "@/components/layouts/errorBoundary/Fallback";
+
+const ErrorPage = (): React.ReactNode => {
+  return <Fallback />;
+};
+
+export default ErrorPage;
---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+import { NotFound } from "@/components/layouts/not-found/NotFound";
+
+const NotFoundPage = (): React.ReactNode => {
+  return <NotFound />;
+};
+
+export default NotFoundPage;
