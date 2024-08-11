dcba17b8ebf0896baef1c975550349311a5f4b1f
panakir <panakir.webdev@gmail.com>
2024-08-11 13:38:51 +0300

    feat: setup remix js (Sun, Aug 11, 2024 13:38)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { vitePlugin as remix } from "@remix-run/dev";
 import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [remix()],
   css: {
     devSourcemap: true
   },
