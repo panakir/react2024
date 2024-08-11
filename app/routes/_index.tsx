1cfdb9a26995fe2bcb2c736a002d07928f354367
panakir <panakir.webdev@gmail.com>
2024-08-11 17:43:37 +0300

    feat: implement server side requests to api (Sun, Aug 11,  2024 17:43)

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,23 +1,33 @@
 import React from "react";
 import { Main } from "../../src/components/layouts/main/Main";
 import { LoaderFunction } from "@remix-run/node";
-import { BASE_URL } from "../constants";
 import { useLoaderData } from "@remix-run/react";
+import { ResponseFromApi } from "../../src/shared/types";
+import { getCharacters } from "../api/api";
+import { Header } from "../../src/components/layouts/header/Header";
+import { Footer } from "../../src/components/layouts/footer/Footer";
 
-export const loader: LoaderFunction = async (): Promise<object> => {
-  const response = await fetch(`${BASE_URL}`)
-    .then((res) => res.json())
-    .catch((error) => {
-      throw new Error("Fetching is failed: " + error.message);
-    });
+export const loader: LoaderFunction = async ({
+  request,
+}): Promise<ResponseFromApi> => {
+  const url = new URL(request.url);
+  const page = url.searchParams.get("page") ?? "1";
+  const query = url.searchParams.get("search") ?? "";
 
+  const response = await getCharacters(query, page);
   return response;
 };
 
 const MainPage = (): React.ReactNode => {
   const data = useLoaderData<typeof loader>();
 
-  return <Main data={data} />;
+  return (
+    <>
+      <Header />
+      <Main data={data} />;
+      <Footer />
+    </>
+  );
 };
 
 export default MainPage;
--- app/routes/details.$id.tsx
+++ app/routes/details.$id.tsx
@@ -1,13 +1,43 @@
 import React from "react";
-import { App } from "../../src/components/app/App";
-import { Provider } from "react-redux";
-import { store } from "../../src/store/store";
+import { Footer } from "../../src/components/layouts/footer/Footer";
+import { Header } from "../../src/components/layouts/header/Header";
+import { Main } from "../../src/components/layouts/main/Main";
+import { LoaderFunction } from "@remix-run/node";
+import { getCharacterById, getCharacters } from "../api/api";
+import { Character, ResponseFromApi } from "../../src/shared/types";
+import { useLoaderData } from "@remix-run/react";
+
+type DetailsLoaderProps = {
+  response: ResponseFromApi;
+  detailsResponse: Character;
+};
+
+export const loader: LoaderFunction = async ({
+  params,
+  request,
+}): Promise<DetailsLoaderProps> => {
+  const id = params.id ?? "";
+  const url = new URL(request.url);
+  const page = url.searchParams.get("page") ?? "1";
+  const query = url.searchParams.get("search") ?? "";
+
+  const detailsResponse = await getCharacterById(id);
+  const response = await getCharacters(query, page);
+
+  return { response, detailsResponse };
+};
 
 const DetailsPage = (): React.ReactNode => {
+  const { response, detailsResponse } = useLoaderData<typeof loader>();
   return (
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <>
+      <Header />
+      <Main
+        data={response}
+        detailsData={detailsResponse}
+      />
+      <Footer />
+    </>
   );
 };
 
