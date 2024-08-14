27ff3644caf96d2c5326941606a8cca5a19e4e97
panakir <panakir.webdev@gmail.com>
2024-08-14 12:16:24 +0300

    chore: setup project configuration (wen, Aug 12:14, 2024 12:15)

---
+++ eslint.config.mjs
@@ -0,0 +1,88 @@
+import { fixupConfigRules } from "@eslint/compat";
+import reactRefresh from "eslint-plugin-react-refresh";
+import reactCompiler from "eslint-plugin-react-compiler";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ["**/dist", "**/.eslintrc.cjs"],
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react-hooks/recommended",
+      "prettier"
+    )
+  ),
+  {
+    plugins: {
+      "react-refresh": reactRefresh,
+      "react-compiler": reactCompiler,
+      prettier,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      parser: tsParser,
+    },
+
+    rules: {
+      "react-compiler/react-compiler": "error",
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "no-empty-function": "error",
+      "@typescript-eslint/no-empty-function": "error",
+      "@typescript-eslint/no-empty-interface": "error",
+      "@typescript-eslint/array-type": "error",
+      "no-console": "warn",
+      "no-inline-comments": "error",
+
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+          allowExportNames: ["loader"],
+        },
+      ],
+
+      "no-duplicate-imports": [
+        "error",
+        {
+          includeExports: true,
+        },
+      ],
+
+      "no-empty": "error",
+      "no-undefined": "error",
+
+      "prefer-destructuring": [
+        "error",
+        {
+          array: true,
+          object: true,
+        },
+      ],
+
+      "require-await": "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error"],
+    },
+  },
+];
